cc31607a9d5c4842b87e259f8ab2b8a042d95960
EgydioBNeto <egydiobolonhezi@gmail.com>
2024-07-09 18:01:37 -0300

    fix

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -8,9 +8,13 @@ import { prisma } from '../lib/prisma'
 import { ClientError } from '../errors/client-error'
 import { env } from '../env'
 
+interface Participant {
+  id: string;
+  email: string;
+}
 export async function confirmTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
-    '/trips/:tripId/confirm',
+    "/trips/:tripId/confirm",
     {
       schema: {
         params: z.object({
@@ -19,7 +23,7 @@ export async function confirmTrip(app: FastifyInstance) {
       },
     },
     async (request, reply) => {
-      const { tripId } = request.params
+      const { tripId } = request.params;
 
       const trip = await prisma.trip.findUnique({
         where: {
@@ -29,37 +33,37 @@ export async function confirmTrip(app: FastifyInstance) {
           participants: {
             where: {
               is_owner: false,
-            }
-          }
-        }
-      })
+            },
+          },
+        },
+      });
 
       if (!trip) {
-        throw new ClientError('Trip not found.')
+        throw new ClientError("Trip not found.");
       }
 
       if (trip.is_confirmed) {
-        return reply.redirect(`${env.WEB_BASE_URL}/trips/${tripId}`)
+        return reply.redirect(`${env.WEB_BASE_URL}/trips/${tripId}`);
       }
 
       await prisma.trip.update({
         where: { id: tripId },
         data: { is_confirmed: true },
-      })
+      });
 
-      const formattedStartDate = dayjs(trip.starts_at).format('LL')
-      const formattedEndDate = dayjs(trip.ends_at).format('LL')
+      const formattedStartDate = dayjs(trip.starts_at).format("LL");
+      const formattedEndDate = dayjs(trip.ends_at).format("LL");
 
-      const mail = await getMailClient()
+      const mail = await getMailClient();
 
       await Promise.all(
-        trip.participants.map(async (participant) => {
-          const confirmationLink = `${env.API_BASE_URL}/participants/${participant.id}/confirm`
+        trip.participants.map(async (participant: Participant) => {
+          const confirmationLink = `${env.API_BASE_URL}/participants/${participant.id}/confirm`;
 
           const message = await mail.sendMail({
             from: {
-              name: 'Equipe plann.er',
-              address: 'oi@plann.er',
+              name: "Equipe plann.er",
+              address: "oi@plann.er",
             },
             to: participant.email,
             subject: `Confirme sua presença na viagem para ${trip.destination} em ${formattedStartDate}`,
@@ -76,13 +80,13 @@ export async function confirmTrip(app: FastifyInstance) {
               <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
             </div>
           `.trim(),
-          })
-    
-          console.log(nodemailer.getTestMessageUrl(message))
-        })
-      )
+          });
 
-      return reply.redirect(`${env.WEB_BASE_URL}/trips/${tripId}`)
-    },
-  )
+          console.log(nodemailer.getTestMessageUrl(message));
+        })
+      );
+
+      return reply.redirect(`${env.WEB_BASE_URL}/trips/${tripId}`);
+    }
+  );
 }
--- src/routes/get-activities.ts
+++ src/routes/get-activities.ts
@@ -1,13 +1,20 @@
-import type { FastifyInstance } from 'fastify'
-import type { ZodTypeProvider } from 'fastify-type-provider-zod'
-import { z } from 'zod'
-import { prisma } from '../lib/prisma'
-import { dayjs } from '../lib/dayjs'
-import { ClientError } from '../errors/client-error'
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { dayjs } from "../lib/dayjs";
+import { ClientError } from "../errors/client-error";
+
+interface Activity {
+  id: string;
+  title: string;
+  occurs_at: Date;
+  trip_id: string;
+}
 
 export async function getActivities(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
-    '/trips/:tripId/activities',
+    "/trips/:tripId/activities",
     {
       schema: {
         params: z.object({
@@ -16,37 +23,42 @@ export async function getActivities(app: FastifyInstance) {
       },
     },
     async (request) => {
-      const { tripId } = request.params
+      const { tripId } = request.params;
 
       const trip = await prisma.trip.findUnique({
         where: { id: tripId },
-        include: { 
+        include: {
           activities: {
             orderBy: {
-              occurs_at: 'asc',
-            }
-          } 
+              occurs_at: "asc",
+            },
+          },
         },
-      })
+      });
 
       if (!trip) {
-        throw new ClientError('Trip not found')
+        throw new ClientError("Trip not found");
       }
 
-      const differenceInDaysBetweenTripStartAndEnd = dayjs(trip.ends_at).diff(trip.starts_at, 'days')
+      const differenceInDaysBetweenTripStartAndEnd = dayjs(trip.ends_at).diff(
+        trip.starts_at,
+        "days"
+      );
 
-      const activities = Array.from({ length: differenceInDaysBetweenTripStartAndEnd + 1 }).map((_, index) => {
-        const date = dayjs(trip.starts_at).add(index, 'days')
+      const activities = Array.from({
+        length: differenceInDaysBetweenTripStartAndEnd + 1,
+      }).map((_, index) => {
+        const date = dayjs(trip.starts_at).add(index, "days");
 
         return {
           date: date.toDate(),
-          activities: trip.activities.filter(activity => {
-            return dayjs(activity.occurs_at).isSame(date, 'day')
-          })
-        }
-      })
+          activities: trip.activities.filter((activity: Activity) => {
+            return dayjs(activity.occurs_at).isSame(date, "day");
+          }),
+        };
+      });
 
-      return { activities }
-    },
-  )
+      return { activities };
+    }
+  );
 }
