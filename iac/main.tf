97366be69b8859f1e9ab0096bab0e7c4b58cbb05
wladimirgrf <wladimir.code@gmail.com>
2024-07-11 09:18:17 -0300

    NLW Journey | Aula 03 | pt 1

---
+++ iac/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.40.0"
+    }
+  }
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "application-tf-state"
+    key            = "terraform.tfstate"
+    dynamodb_table = "app-tf-state-lock"
+    encrypt        = true
+
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source         = "./modules/vpc"
+  prefix         = var.prefix
+  vpc_cidr_block = var.vpc_cidr_block
+}
+
+module "eks" {
+  source         = "./modules/eks"
+  prefix         = var.prefix
+  vpc_id         = module.vpc.vpc_id
+  subnet_ids     = module.vpc.subnet_ids
+  cluster_name   = var.cluster_name
+  retention_days = var.retention_days
+  desired_size   = var.desired_size
+  max_size       = var.max_size
+  min_size       = var.min_size
+}
